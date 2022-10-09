b0b2dce6fff5ec479aeb5087db114119a166ab50
Halgurd Hussein <halgurd.hussein@heroes.nl>
2022-10-09 11:10:51 +0200

    fixed mobile

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -19,9 +19,9 @@ import Webpack from '../assets/webpack.png';
 
 const Skills = () => {
   return (
-    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+    <div name='skills' className='pt-96 sm:pt-2 w-full h-screen bg-[#0a192f] text-gray-300'>
       {/* Container */}
-      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+      <div className='max-w-[1000px] mx-auto pt-32 sm:pt-2 p-4 flex flex-col justify-center w-full h-full'>
           <div>
               <p className='text-4xl font-bold inline border-b-4 border-[#dd3333] '>Skills</p>
               <p className='py-4'>// These are the technologies I've worked with</p>
