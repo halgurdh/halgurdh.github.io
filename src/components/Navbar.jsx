b564db1c42de9656814d71a170ac6bcaa7f08e2d
Halgurd Hussein <halgurd.hussein@heroes.nl>
2022-10-13 16:48:50 +0200

    added more skills

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   FaBars,
   FaTimes,
@@ -6,40 +6,40 @@ import {
   FaLinkedin,
   FaSpotify,
   FaInstagram,
-} from 'react-icons/fa';
-import { Link } from 'react-scroll';
+} from "react-icons/fa";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
-        <h1 className='text-2xl text-[#dd3333]'>Halgurdh</h1>
+        <h1 className="text-2xl text-[#dd3333]">Halgurdh</h1>
       </div>
 
       {/* menu */}
-      <ul className='hidden md:flex'>
+      <ul className="hidden md:flex">
         <li>
-          <Link to='home' smooth={true} duration={500}>
+          <Link to="home" smooth={true} duration={500}>
             Home
           </Link>
         </li>
         <li>
-          <Link to='skills' smooth={true} duration={500}>
+          <Link to="skills" smooth={true} duration={500}>
             Skills
           </Link>
         </li>
         <li>
-          <Link to='work' smooth={true} duration={500}>
+          <Link to="work" smooth={true} duration={500}>
             Work
           </Link>
         </li>
       </ul>
 
       {/* Hamburger */}
-      <div onClick={handleClick} className='md:hidden z-10'>
+      <div onClick={handleClick} className="md:hidden z-10">
         {!nav ? <FaBars /> : <FaTimes />}
       </div>
 
@@ -47,69 +47,69 @@ const Navbar = () => {
       <ul
         className={
           !nav
-            ? 'hidden'
-            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
         }
       >
-        <li className='py-6 text-4xl'>
-          <Link onClick={handleClick} to='home' smooth={true} duration={500}>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
             Home
           </Link>
         </li>
-        <li className='py-6 text-4xl'>
-          {' '}
-          <Link onClick={handleClick} to='about' smooth={true} duration={500}>
+        <li className="py-6 text-4xl">
+          {" "}
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
             About
           </Link>
         </li>
-        <li className='py-6 text-4xl'>
-          {' '}
-          <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
+        <li className="py-6 text-4xl">
+          {" "}
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
             Skills
           </Link>
         </li>
-        <li className='py-6 text-4xl'>
-          {' '}
-          <Link onClick={handleClick} to='work' smooth={true} duration={500}>
+        <li className="py-6 text-4xl">
+          {" "}
+          <Link onClick={handleClick} to="work" smooth={true} duration={500}>
             Work
           </Link>
         </li>
       </ul>
 
       {/* Social icons */}
-      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
         <ul>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
             <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='https://www.linkedin.com/in/halgurdhussein/'
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://www.linkedin.com/in/halgurdhussein/"
               target="_blank"
             >
               Linkedin <FaLinkedin size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#dd3333]'>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#dd3333]">
             <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='https://github.com/halgurdh/'
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://github.com/halgurdh/"
               target="_blank"
             >
               Github <FaGithub size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-green-600'>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-green-600">
             <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='https://open.spotify.com/artist/27axU7fE93Z7h8OhRFrD4r'
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://open.spotify.com/artist/27axU7fE93Z7h8OhRFrD4r"
               target="_blank"
             >
               Spotify <FaSpotify size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-purple-600'>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-purple-600">
             <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='https://www.instagram.com/chilledpunks/'
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://www.instagram.com/chilledpunks/"
               target="_blank"
             >
               Instagram <FaInstagram size={30} />
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,98 +1,243 @@
-import React from 'react';
+import React from "react";
 
-import HTML from '../assets/html.png';
-import CSS from '../assets/css.png';
-import JavaScript from '../assets/javascript.png';
-import ReactImg from '../assets/react.png';
-import DevOps from '../assets/devops.png';
-import GitHub from '../assets/github.png';
-import Tailwind from '../assets/tailwind.png';
-import CSharp from '../assets/csharp.png';
-import Azure from '../assets/azure.png';
-import NodeJS from '../assets/node.png';
-import VSCode from '../assets/vscode.png';
-import VS2022 from '../assets/vs2022.png';
-import NETCore from '../assets/netcore.png';
-import Bootstrap from '../assets/bootstrap.png';
-import Bash from '../assets/bash.png';
-import Webpack from '../assets/webpack.png';
+import HTML from "../assets/html.png";
+import CSS from "../assets/css.png";
+import JavaScript from "../assets/javascript.png";
+import ReactImg from "../assets/react.png";
+import DevOps from "../assets/devops.png";
+import GitHub from "../assets/github.png";
+import Tailwind from "../assets/tailwind.png";
+import CSharp from "../assets/csharp.png";
+import Azure from "../assets/azure.png";
+import NodeJS from "../assets/node.png";
+import VSCode from "../assets/vscode.png";
+import VS2022 from "../assets/vs2022.png";
+import NETCore from "../assets/netcore.png";
+import Bootstrap from "../assets/bootstrap.png";
+import Bash from "../assets/bash.png";
+import Webpack from "../assets/webpack.png";
+import Npm from "../assets/npm.png";
+import NUnit from "../assets/nunit.png";
+import TypeScript from "../assets/typescript.png";
+import NextJS from "../assets/nextjs.png";
 
 const Skills = () => {
   return (
-    <div name='skills' className='pt-96 sm:pt-2 w-full h-screen bg-[#0a192f] text-gray-300'>
+    <div
+      name="skills"
+      className="pt-96 sm:pt-2 w-full h-screen bg-[#0a192f] text-gray-300"
+    >
       {/* Container */}
-      <div className='max-w-[1000px] mx-auto pt-32 sm:pt-2 p-4 flex flex-col justify-center w-full h-full'>
-          <div>
-              <p className='text-4xl font-bold inline border-b-4 border-[#dd3333] '>Skills</p>
-              <p className='py-4'>// These are the technologies I've worked with</p>
-          </div>
+      <div className="max-w-[1000px] mx-auto pt-96 sm:pt-2 p-4 flex flex-col justify-center w-full h-full">
+        <div>
+          <p className="text-4xl font-bold inline border-b-4 border-[#dd3333] ">
+            Skills
+          </p>
+          <p className="py-4">// These are the technologies I've worked with</p>
+        </div>
 
-          <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-              <div className='shadow-md shadow-[#040c16] group'>
-                  <img className='group-hover:animate-bounce m w-20 m mx-auto' src={HTML} alt="HTML icon" />
-                  <p className='my-4 group-hover:text-bold group-hover:scale-125'>HTML</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] group'>
-                  <img className='group-hover:animate-bounce m w-20 m mx-auto' src={CSS} alt="CSS icon" />
-                  <p className='my-4 group-hover:text-bold group-hover:scale-125'>CSS</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] group'>
-                  <img className='group-hover:animate-bounce m w-20 m mx-auto' src={JavaScript} alt="JavaScript icon" />
-                  <p className='my-4 group-hover:text-bold group-hover:scale-125'>JAVASCRIPT</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] group'>
-                  <img className='group-hover:animate-bounce m w-20 m mx-auto' src={ReactImg} alt="React icon" />
-                  <p className='my-4 group-hover:text-bold group-hover:scale-125'>REACTJS</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] group'>
-                  <img className='group-hover:animate-bounce m w-20 m mx-auto' src={GitHub} alt="GitHub icon" />
-                  <p className='my-4 group-hover:text-bold group-hover:scale-125'>GITHUB</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] group'>
-                  <img className='group-hover:animate-bounce m w-20 m mx-auto' src={DevOps} alt="DevOps icon" />
-                  <p className='my-4 group-hover:text-bold group-hover:scale-125'>DEVOPS</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] group'>
-                  <img className='group-hover:animate-bounce m w-20 m mx-auto' src={Tailwind} alt="Tailwind icon" />
-                  <p className='my-4 group-hover:text-bold group-hover:scale-125'>TAILWIND</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] group'>
-                  <img className='group-hover:animate-bounce m w-20 m mx-auto' src={CSharp} alt="CSharp icon" />
-                  <p className='my-4 group-hover:text-bold group-hover:scale-125'>CSHARP</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] group'>
-                  <img className='group-hover:animate-bounce m w-20 m mx-auto' src={Azure} alt="Azure icon" />
-                  <p className='my-4 group-hover:text-bold group-hover:scale-125'>AZURE</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] group'>
-                  <img className='group-hover:animate-bounce m w-20 m mx-auto' src={NodeJS} alt="NodeJS icon" />
-                  <p className='my-4 group-hover:text-bold group-hover:scale-125'>NODEJS</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] group'>
-                  <img className='group-hover:animate-bounce m w-20 m mx-auto' src={VSCode} alt="VSCode icon" />
-                  <p className='my-4 group-hover:text-bold group-hover:scale-125'>VSCODE</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] group'>
-                  <img className='group-hover:animate-bounce m w-20 m mx-auto' src={VS2022} alt="VS2022 icon" />
-                  <p className='my-4 group-hover:text-bold group-hover:scale-125'>VS2022</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] group'>
-                  <img className='group-hover:animate-bounce m w-20 m mx-auto' src={NETCore} alt="NETCore icon" />
-                  <p className='my-4 group-hover:text-bold group-hover:scale-125'>DOTNET</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] group'>
-                  <img className='group-hover:animate-bounce m w-20 m mx-auto' src={Bash} alt="Bash icon" />
-                  <p className='my-4 group-hover:text-bold group-hover:scale-125'>BASH</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] group'>
-                  <img className='group-hover:animate-bounce m w-20 m mx-auto' src={Bootstrap} alt="Bootstrap icon" />
-                  <p className='my-4 group-hover:text-bold group-hover:scale-125'>BOOTSTRAP</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] group'>
-                  <img className='group-hover:animate-bounce m w-20 m mx-auto' src={Webpack} alt="Webpack icon" />
-                  <p className='my-4 group-hover:text-bold group-hover:scale-125'>WEBPACK</p>
-              </div>
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+          <div className="shadow-md shadow-[#040c16] group">
+            <img
+              className="group-hover:animate-bounce m w-20 m mx-auto"
+              src={HTML}
+              alt="HTML icon"
+            />
+            <p className="my-4 group-hover:text-bold group-hover:scale-125">
+              HTML
+            </p>
           </div>
+          <div className="shadow-md shadow-[#040c16] group">
+            <img
+              className="group-hover:animate-bounce m w-20 m mx-auto"
+              src={CSS}
+              alt="CSS icon"
+            />
+            <p className="my-4 group-hover:text-bold group-hover:scale-125">
+              CSS
+            </p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] group">
+            <img
+              className="group-hover:animate-bounce m w-20 m mx-auto"
+              src={JavaScript}
+              alt="JavaScript icon"
+            />
+            <p className="my-4 group-hover:text-bold group-hover:scale-125">
+              JAVASCRIPT
+            </p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] group">
+            <img
+              className="group-hover:animate-bounce m w-20 m mx-auto"
+              src={ReactImg}
+              alt="React icon"
+            />
+            <p className="my-4 group-hover:text-bold group-hover:scale-125">
+              REACTJS
+            </p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] group">
+            <img
+              className="group-hover:animate-bounce m w-20 m mx-auto"
+              src={GitHub}
+              alt="GitHub icon"
+            />
+            <p className="my-4 group-hover:text-bold group-hover:scale-125">
+              GITHUB
+            </p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] group">
+            <img
+              className="group-hover:animate-bounce m w-20 m mx-auto"
+              src={DevOps}
+              alt="DevOps icon"
+            />
+            <p className="my-4 group-hover:text-bold group-hover:scale-125">
+              DEVOPS
+            </p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] group">
+            <img
+              className="group-hover:animate-bounce m w-20 m mx-auto"
+              src={Tailwind}
+              alt="Tailwind icon"
+            />
+            <p className="my-4 group-hover:text-bold group-hover:scale-125">
+              TAILWIND
+            </p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] group">
+            <img
+              className="group-hover:animate-bounce m w-20 m mx-auto"
+              src={CSharp}
+              alt="CSharp icon"
+            />
+            <p className="my-4 group-hover:text-bold group-hover:scale-125">
+              CSHARP
+            </p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] group">
+            <img
+              className="group-hover:animate-bounce m w-20 m mx-auto"
+              src={Azure}
+              alt="Azure icon"
+            />
+            <p className="my-4 group-hover:text-bold group-hover:scale-125">
+              AZURE
+            </p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] group">
+            <img
+              className="group-hover:animate-bounce m w-20 m mx-auto"
+              src={NodeJS}
+              alt="NodeJS icon"
+            />
+            <p className="my-4 group-hover:text-bold group-hover:scale-125">
+              NODEJS
+            </p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] group">
+            <img
+              className="group-hover:animate-bounce m w-20 m mx-auto"
+              src={VSCode}
+              alt="VSCode icon"
+            />
+            <p className="my-4 group-hover:text-bold group-hover:scale-125">
+              VSCODE
+            </p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] group">
+            <img
+              className="group-hover:animate-bounce m w-20 m mx-auto"
+              src={VS2022}
+              alt="VS2022 icon"
+            />
+            <p className="my-4 group-hover:text-bold group-hover:scale-125">
+              VS2022
+            </p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] group">
+            <img
+              className="group-hover:animate-bounce m w-20 m mx-auto"
+              src={NETCore}
+              alt="NETCore icon"
+            />
+            <p className="my-4 group-hover:text-bold group-hover:scale-125">
+              DOTNET
+            </p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] group">
+            <img
+              className="group-hover:animate-bounce m w-20 m mx-auto"
+              src={Bash}
+              alt="Bash icon"
+            />
+            <p className="my-4 group-hover:text-bold group-hover:scale-125">
+              BASH
+            </p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] group">
+            <img
+              className="group-hover:animate-bounce m w-20 m mx-auto"
+              src={Bootstrap}
+              alt="Bootstrap icon"
+            />
+            <p className="my-4 group-hover:text-bold group-hover:scale-125">
+              BOOTSTRAP
+            </p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] group">
+            <img
+              className="group-hover:animate-bounce m w-20 m mx-auto"
+              src={Webpack}
+              alt="Webpack icon"
+            />
+            <p className="my-4 group-hover:text-bold group-hover:scale-125">
+              WEBPACK
+            </p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] group">
+            <img
+              className="group-hover:animate-bounce m w-20 m mx-auto"
+              src={Npm}
+              alt="Npm icon"
+            />
+            <p className="my-4 group-hover:text-bold group-hover:scale-125">
+              NPM
+            </p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] group">
+            <img
+              className="group-hover:animate-bounce m w-20 m mx-auto"
+              src={NUnit}
+              alt="NUnit icon"
+            />
+            <p className="my-4 group-hover:text-bold group-hover:scale-125">
+              NUnit
+            </p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] group">
+            <img
+              className="group-hover:animate-bounce m w-20 m mx-auto"
+              src={TypeScript}
+              alt="TypeScript icon"
+            />
+            <p className="my-4 group-hover:text-bold group-hover:scale-125">
+              TypeScript
+            </p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] group">
+            <img
+              className="group-hover:animate-bounce m w-20 m mx-auto"
+              src={NextJS}
+              alt="NextJS icon"
+            />
+            <p className="my-4 group-hover:text-bold group-hover:scale-125">
+              NextJS
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
