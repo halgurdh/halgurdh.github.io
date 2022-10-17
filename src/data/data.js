31467bdffa0c209986667fbe3a9ee7d6b219cefe
halgurd <halgurdh@gmail.com>
2022-10-17 18:18:04 +0200

    added chatbot project

--- src/data/data.js
+++ src/data/data.js
@@ -1,38 +1,39 @@
-export const data=[
-    {
-        id:1,
-        name:"NextJS Movies",
-        github:"https://github.com/halgurdh/nextjs-tvshows",
-        live: "https://nextjs-tvshows.vercel.app/"
-        
-    },
-    {
-        id:2,
-        name:"PhaserJS Games",
-        github:"https://github.com/halgurdh/phaser-template",
-        live: "https://www.minitoongames.com/portfolio/"
-    },
-    {
-        id:3,
-        name:"C# + ReactJS Budgeting App",
-        github:"https://github.com/halgurdh/budgetAPI-react-dotnet",
-    },
-    {
-        id:4,
-        name:"ReactJS TravelSite",
-        github:"https://github.com/halgurdh/react-site",
-    },
-    {
-        id:5,
-        name:"C# + ReactJS Posts",
-        github:"https://github.com/halgurdh/react-dotnet",
-    },
-    {
-        id:6,
-        name:"TruffleJS Blockchain",
-        github:"https://github.com/halgurdh/blockchain-as-a-service",
-    },
-    
-
-
-]
+export const data = [
+  {
+    id: 1,
+    name: "NextJS Movies",
+    github: "https://github.com/halgurdh/nextjs-tvshows",
+    live: "https://nextjs-tvshows.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "PhaserJS Games",
+    github: "https://github.com/halgurdh/phaser-template",
+    live: "https://www.minitoongames.com/portfolio/",
+  },
+  {
+    id: 3,
+    name: "C# + ReactJS Budgeting App",
+    github: "https://github.com/halgurdh/budgetAPI-react-dotnet",
+  },
+  {
+    id: 4,
+    name: "ReactJS TravelSite",
+    github: "https://github.com/halgurdh/react-site",
+  },
+  {
+    id: 5,
+    name: "C# + ReactJS Posts",
+    github: "https://github.com/halgurdh/react-dotnet",
+  },
+  {
+    id: 6,
+    name: "TruffleJS Blockchain",
+    github: "https://github.com/halgurdh/blockchain-as-a-service",
+  },
+  {
+    id: 7,
+    name: "Microsoft ChatBot Demo in C#",
+    github: "https://github.com/halgurdh/MinitoonChatBot",
+  },
+];
